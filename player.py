c876e560ec639e35f9d54aa25e04ba482c874911
pmacking <pmacking@users.noreply.github.com>
2020-03-28 11:19:16 -0400

    removed yahtzee.py and finished scoring gameplay

--- player.py
+++ player.py
@@ -1,15 +1,48 @@
 #! python3
+import pyinputplus as pyip
 
 
 class Player:
     def __init__(self, name):
         self.name = name
+        # scoring dictionaries
+        self._scoreDict = {
+            'ones': False, 'twos': False, 'threes': False, 'fours': False,
+            'fives': False, 'sixes': False, 'three of a kind': False,
+            'four of a kind': False, 'full house': False,
+            'small straight': False, 'large straight': False, 'yahtzee': False,
+            'chance': False, 'yahtzee bonus': False,
+            }
 
         self._topScore = 0
         self._topBonusScore = 0
         self._totalTopScore = 0
         self._totalBottomScore = 0
         self._grandTotalScore = 0
+
+    def selectScore(self):
+        '''
+        Allows player to select the scoring option for final dice roll
+        '''
+        scoreOptions = []
+        doubleCheck = False
+
+        # build list of scoring options from player's scoring dictionary
+        while doubleCheck is False:
+            for i, option in enumerate(self._scoreDict):
+                if self._scoreDict[option] is False:
+                    scoreOptions.append(option)
+
+            # remove yahtzee bonus if yahtzee hasn't been scored yet
+            if self._scoreDict['yahtzee'] is False:
+                scoreOptions.remove('yahtzee bonus')
+
+            # let player select option
+            scoreSelected = pyip.inputMenu(scoreOptions, numbered=True)
+            # confirm selection
+            if pyip.inputYesNo(prompt=f"\n{self.name.upper()} are you sure you want to select {scoreSelected}?\n") == 'yes':
+                doubleCheck = True
+        return scoreSelected
 
     def addScoreDict(self, scoreSelected, score):
         '''
@@ -29,56 +62,42 @@ class Player:
         '''
         bonusThreshold = 63
 
-        if self.getTopScore() >= bonusThreshold:
-            self._scoreDict['bonus'] = 50
-        else:
-            self._scoreDict['bonus'] = 0
-
-        self._topBonusScore = self._scoreDict['bonus']
-
-    def getTopScore(self):
-        '''
-        Returns current top score
-        '''
-        return self._topScore
+        if self._topScore >= bonusThreshold and self._topBonusScore == 0:
+            self._topBonusScore = 35
 
     def printScoreDict(self):
         '''
         Prints the scoring dictionary for the player
         '''
         for key, value in self._scoreDict.items():
-            print(f'{key}: {value}')
+            print(f'{key.rjust(15)}: {value*1}')
 
-#     def createPlayerDict(playerDict):
-#     '''
-#     Creates playerDict containing player(s) scoring dictionary
-#     Args: None
-#     Returns: playerDict
-#     '''
-#     numberOfPlayers = pyip.inputInt(prompt='\nEnter number of players (1-4):\n', min=1, max=4)
-#     for _ in range(numberOfPlayers):
-#         playerDict[pyip.inputStr(prompt='\nEnter name of player '+str(_+1)+':\n')] = {'scoreTop': {'Ones': {'ref': 1, 'score': False}, 'Twos': {'ref': 2, 'score': False}, 'Threes': {'ref': 3, 'score': False}, 'Fours': {'ref': 4, 'score': False}, 'Fives': {'ref': 5, 'score': False}, 'Sixes': {'ref': 6, 'score': False}}, 'scoreBottom': {'Three of a kind': False, 'Four of a kind': False, 'Full house': False, 'Small straight': False, 'Large straight': False, 'Yahtzee': False, 'Chance': False, 'Yahtzee bonus': 0}, 'totalScore': {'Sum of upper': False, 'Bonus': False, 'Total upper': False, 'Total bottom': False, }, 'Grand total': 0}
-#     return playerDict
+    def printTopScore(self):
+        '''
+        Prints the top score (before bonus)
+        '''
+        print(f'Top Score: '.rjust(20) + f'{self._topScore}')
 
-#     # select scoring after final dice roll of current player
-# def selectScore(player, playerDict, diceDict, scoreSelected):
-#     '''
-#     Allows player to select which scoring option to apply dice results to within calcScore.
-#     Args: player, playerDict, diceDict, scoreSelected
-#     Returns: playerDict, scoreSelected, diceDict
-#     '''
-#     # present and select available (False) scoring options in playerDict
-#     doubleCheck = 0
-#     while doubleCheck == 0:
-#         scoreOptions = []
-#         for k in playerDict[player]['scoreTop']:
-#             if playerDict[player]['scoreTop'][k]['score'] is False:
-#                 scoreOptions.append(k)
-#         for k in playerDict[player]['scoreBottom']:
-#             if playerDict[player]['scoreBottom'][k] is False:
-#                 scoreOptions.append(k)
-#         scoreSelected = pyip.inputMenu(scoreOptions, numbered=True)
-#         # confirm selection
-#         if pyip.inputYesNo(prompt=f"\n{player} are you sure you want to select {scoreSelected}?\n") == 'yes':
-#             doubleCheck = 1
-#     return playerDict, scoreSelected, diceDict
+    def printTopBonusScore(self):
+        '''
+        Prints the top bonus score
+        '''
+        print(f'Top Bonus Score: '.rjust(20) + f'{self._topBonusScore}')
+
+    def printTotalTopScore(self):
+        '''
+        Prints the total top score for the player
+        '''
+        print(f'Total Top Score: '.rjust(20) + f'{self._totalTopScore}')
+
+    def printTotalBottomScore(self):
+        '''
+        Prints the total top score for the player
+        '''
+        print(f'Total Bottom Score: '.rjust(20) + f'{self._totalBottomScore}')
+
+    def printGrandTotalScore(self):
+        '''
+        Prints the total top score for the player
+        '''
+        print(f'GRAND TOTAL: '.rjust(20) + f'{self._grandTotalScore}\n')
